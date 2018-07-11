8c7c7739060eb5c1142d9a4c7b9b85a0fded19e0
m8itsjill <jillyusi@icloud.com>
2018-07-11 16:17:16 +0000

    automatically backed up by learn

--- curfew.rb
+++ curfew.rb
@@ -1,6 +1,9 @@
-def 
-if 11 
-  puts "You're in trouble"
-else 13  >= 11
-  puts "You're in trouble."
+def simple_curfew_checker(time)
+  if = 11
+    puts "You're in trouble! Better get home quick!"
+  else 12 >= 11
+    puts "You're in trouble! Better get home quick!"
+  elsif 10 <= 11
+    puts "You're not in trouble!"
+  end
 end
